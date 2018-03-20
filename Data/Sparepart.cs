c02773f5bfcb3f658f939c6fe1119f73543d86bd
Vitali Lozyuk <babtsoualiaksandr@gmail.com>
2018-03-20 09:53:16 +0300

    Редактирование картинок в запчастях

---
+++ Data/Sparepart.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fresenius.Data
+{
+    public class Sparepart
+    {
+        public int Id { get; set; }
+        [Display(Name = "Номер запчасти")]
+        public string Number { get; set; }
+        [Display(Name = "Наименование En.")]
+        public string NameEn { get; set; }
+        [Display(Name = "Наименование Rus. ")]
+        public string NameRus { get; set; }
+
+        
+        public int? EquipmentId { get; set; }
+        [Display(Name = "Оборуд. ")]
+        public Equipment Equipment { get; set; }
+
+        [Display(Name = "Краткое_Описание_Запчасти")]
+        public string Description { get; set; }
+
+
+        public int? ManufacturerId { get; set; }
+        [Display(Name = "Производитель")]
+        public Manufacturer Manufacturer { get; set; }
+
+        
+        public int? CountryId { get; set; }
+        [Display(Name = "Страна")]
+        public Country Country { get; set; }
+
+        [Display(Name = "Фото ")]
+        public string Image { get; set; }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,45 @@
+﻿//// Write your JavaScript code.
+$(document).ready(function () {
+    $('#IndexTable').DataTable();
+});
+
+/// Предварительный просмотр картинок
+$('#img').change(function () {
+    var input = $(this)[0];
+    if (input.files && input.files[0]) {
+        if (input.files[0].type.match('image.*')) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#img-preview').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        } else {
+            console.log('ошибка, не изображение');
+        }
+    } else {
+        console.log('хьюстон у нас проблема');
+
+    }
+    //заполнить строку ввода пути после изменения файла картинки в Edit
+    //<input type="text" class="bginput" name="searchuser" id="userfield_txt" size="35" value="" style="width:250px" autocomplete="off" />
+    //<input asp-for="Image" class="form-control" id="InputImage" />
+    (function bookmark() {
+        document.getElementById('InputImage').value = '~/images/'+input.files[0].name
+
+    })();
+});
+
+$('#reset-img-preview').click(function () {
+    $('#img').val('');
+    $('#img-preview').attr('src', 'default-preview.jpg');
+});
+
+$('#form').bind('reset', function () {
+    $('#img-preview').attr('src', 'default-preview.jpg');
+});
+//// **конец
+
+
+
+
+
